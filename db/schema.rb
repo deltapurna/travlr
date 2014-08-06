f32b0abd18aec04cbacd5e256c87a4c81a83b1bb
delta <d@qiscus.com>
2014-08-06 14:10:54 +0700

    fixed column type

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806013859) do
+ActiveRecord::Schema.define(version: 20140806070039) do
 
   create_table "places", force: true do |t|
     t.string   "name"
-    t.float    "latitude"
-    t.float    "longitude"
+    t.float    "latitude",   limit: 255
+    t.float    "longitude",  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
 end
