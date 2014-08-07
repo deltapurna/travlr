656738dd4d43c8ca2268484ec1bd22c60ccab314
delta <d@qiscus.com>
2014-08-07 11:32:04 +0700

    added token based auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,20 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged In!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render :new
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        format.html do
+          session[:user_id] = user.id
+          redirect_to root_path, notice: "Logged In!"
+        end
+        format.json { render json: user }
+      else
+        format.html do
+          flash.now.alert = "Invalid email or password"
+          render :new
+        end
+        format.json { render json: "Invalid email or password" }
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
+  # Callback
+  before_create :set_auth_token
+
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
 
-  # user.authenticate("password")
-  # nambahin 2 password & password_confirmation
-  # nambahin validasi password & password_confimation
+  def set_auth_token
+    self.auth_token = loop do
+      token = SecureRandom.hex
+      break token unless self.class.exists?(auth_token: token)
+    end
+  end
 end
